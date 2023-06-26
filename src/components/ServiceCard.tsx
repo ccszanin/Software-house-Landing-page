54d52460e544c79d8914bfe50976d52b2c3f05f8
ZaninDe <gabriel.zaninde@gmail.com>
2023-06-25 22:30:55 -0300

    styles review

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -8,17 +8,16 @@ interface ServiceCardProps {
   text: string;
 }
 
-export function ServiceCard(data:ServiceCardProps ) {
-  return(
-    <div className="pt-14 pb-20 pl-16 pr-1 bg-black-600  border-solid border-[0.063rem] rounded-[36px] border-green-500  hover:bg-green-500 py-2  transition duration-300  hover:text-black-600 group">
-     
-     <Image
-              className="mb-[1.875rem]"
-              src={data.img}
-              alt="Icon-1"
-            />
-      <h1 className="text-3xl font-bold leading-[130%] mb-[1.875rem] ">{data.title}</h1>
-      <p className="text-xl leading-[150%] max-w-[17rem]">{data.text}</p>
+export function ServiceCard(data: ServiceCardProps) {
+  return (
+    <div className="p-8 bg-black-600 min-h-[30vh]  border-solid border-[0.063rem] rounded-[36px] border-green-500  hover:bg-green-500 transition duration-300  hover:text-black-600 group">
+      <Image
+        className="mb-[1.875rem] w-6 lg:w-8"
+        src={data.img}
+        alt="Icon-1"
+      />
+      <h1 className="text-md font-bold leading-[130%] mb-[1.875rem] ">{data.title}</h1>
+      <p className="text-sm leading-[150%] max-w-[17rem]">{data.text}</p>
     </div>
 
   )
