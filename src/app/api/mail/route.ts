05d72225bc6f07f5bf83fec2a24185022a6bee9b
ZaninDe <gabriel.zaninde@gmail.com>
2023-07-04 21:52:47 -0300

    send mail with html message

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: NextRequest) {
     from: 'zionsoftwarehouse.dev@gmail.com',
     subject: subject,
     text: text,
-    // html: html
+    html: html
   }
   try {
     await sgMail.send(msg);
